f3b1555d0ed2799e02796295e1f11c2f47e1e2c6
Sachintha_Dissanayaka <95671286+SDDISSANAYAKA@users.noreply.github.com>
2024-11-21 16:05:45 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Todo from './Component/Todo'
+import TodoItems from './Component/TodoItems'
+
+const App = () => {
+  return (
+    <div>
+      <Todo />
+    </div>
+  )
+}
+
+export default App
